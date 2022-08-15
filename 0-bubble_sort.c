9733abd5219824a001ca90ca9f07decbda403d76
Brandon <brandon_scott585@yahoo.com>
2022-08-15 14:02:55 -0700

    Sortring Algorithims and Big O

---
+++ 0-bubble_sort.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+/*
+ * bubble_sort - sort an int array with bubble_sort
+ * @array: array of integers of size
+ * @size: the sze of the array
+ *
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int tmp;
+	size_t i,j;
