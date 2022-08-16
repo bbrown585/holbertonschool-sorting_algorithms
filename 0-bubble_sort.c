2ae6aeeec076974ba259e2ef66a22839f0517247
Brandon <brandon_scott585@yahoo.com>
2022-08-16 07:57:42 -0700

    updating task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "sort.h"
 /*
  * bubble_sort - sort an int array with bubble_sort
  * @array: array of integers of size
@@ -8,5 +9,19 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int tmp;
-	size_t i,j;
+	size_t i,j,k;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0;  j < size - i - 1 ; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				k = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = k;
+				print_array(array, size);
+			}
+		}
+	}
+}
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of intergers in order
+ * @list: list to go through
+ *
+ *
+ */
+void insertion_sort_list(listint_t **list)
+{
