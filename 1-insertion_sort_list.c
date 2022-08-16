a4d1bde7058fc84fd7b0cf0f5c6e52aa2b970f34
Brandon <brandon_scott585@yahoo.com>
2022-08-16 12:08:43 -0700

    retrying task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,7 +12,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *a, *b;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (list == NULL || (*list) == NULL || (*list)->next == NULL)
 		return;
 	a = *list;
 	while (b != NULL)
