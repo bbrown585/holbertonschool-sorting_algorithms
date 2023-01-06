18e74734729e3230c5d5d8a5db7bc21fcf01db91
bbrown585 <brandon_scott585@yahoo.com>
2023-01-06 17:32:13 +0000

    betty installed

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,19 +1,19 @@
 #include "sort.h"
 
-void merge_subarray(int *subarray, int *buffer, size_t start, size_t middle,
+void merge_subarray(int *subarray, int *buff, size_t start, size_t middle,
 		size_t end);
-void merge_sort_recursive(int *subarray, int *buffer, size_t start, size_t end);
+void merge_sort_recursive(int *subarray, int *buff, size_t start, size_t end);
 void merge_sort(int *array, size_t size);
 
 /**
  * merge_subarray - Sort a subarrayay of integers.
  * @subarray: A subarrayay of an array of integers to sort.
- * @buffer: A bufferer to store the sorted subarrayay.
+ * @buff: A buffer to store the sorted subarrayay.
  * @start: The start index of the array.
  * @mid: The middle index of the array.
  * @end: The end index of the array.
  */
-void merge_subarray(int *subarray, int *buffer, size_t start, size_t mid,
+void merge_subarray(int *subarray, int *buff, size_t start, size_t mid,
 		size_t end)
 {
 	size_t i, j, k = 0;
@@ -25,13 +25,13 @@ void merge_subarray(int *subarray, int *buffer, size_t start, size_t mid,
 	print_array(subarray + mid, end - mid);
 
 	for (i = start, j = mid; i < mid && j < end; k++)
-		buffer[k] = (subarray[i] < subarray[j]) ? subarray[i++] : subarray[j++];
+		buff[k] = (subarray[i] < subarray[j]) ? subarray[i++] : subarray[j++];
 	for (; i < mid; i++)
-		buffer[k++] = subarray[i];
+		buff[k++] = subarray[i];
 	for (; j < end; j++)
-		buffer[k++] = subarray[j];
+		buff[k++] = subarray[j];
 	for (i = start, k = 0; i < end; i++)
-		subarray[i] = buffer[k++];
+		subarray[i] = buff[k++];
 
 	printf("[Done]: ");
 	print_array(subarray + start, end - start);
@@ -40,20 +40,20 @@ void merge_subarray(int *subarray, int *buffer, size_t start, size_t mid,
 /**
  * merge_sort_recursive - Implement the merge sort algorithm through recursion.
  * @subarray: A subarrayay of an array of integers to sort.
- * @buffer: A bufferer to store the sorted result.
+ * @buff: A buffer to store the sorted result.
  * @start: The start index of the subarrayay.
  * @end: The end index of the subarrayay.
  */
-void merge_sort_recursive(int *subarray, int *buffer, size_t start, size_t end)
+void merge_sort_recursive(int *subarray, int *buff, size_t start, size_t end)
 {
 	size_t mid;
 
 	if (end - start > 1)
 	{
 		mid = start + (end - start) / 2;
-		merge_sort_recursive(subarray, buffer, start, mid);
-		merge_sort_recursive(subarray, buffer, mid, end);
-		merge_subarray(subarray, buffer, start, mid, end);
+		merge_sort_recursive(subarray, buff, start, mid);
+		merge_sort_recursive(subarray, buff, mid, end);
+		merge_subarray(subarray, buff, start, mid, end);
 	}
 }
 
@@ -67,16 +67,16 @@ void merge_sort_recursive(int *subarray, int *buffer, size_t start, size_t end)
  */
 void merge_sort(int *array, size_t size)
 {
-	int *buffer;
+	int *buff;
 
 	if (array == NULL || size < 2)
 		return;
 
-	buffer = malloc(sizeof(int) * size);
-	if (buffer == NULL)
+	buff = malloc(sizeof(int) * size);
+	if (buff == NULL)
 		return;
 
-	merge_sort_recursive(array, buffer, 0, size);
+	merge_sort_recursive(array, buff, 0, size);
 
-	free(buffer);
+	free(buff);
 }
